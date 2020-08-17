1bb39329f6136fd7e159b44d311d039e10934ea7
GustavoHenriqueDK <gustavo.dickmann123@gmail.com>
2020-08-17 10:57:24 -0300

    - MaskFormatter lib implemented and added in code (password length); - Checkbox added; - Button implemented;

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.passwordgenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.rtoshiro.util.format.SimpleMaskFormatter
+import com.github.rtoshiro.util.format.text.MaskTextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        passwordLengthMask()
+    }
+
+    private fun passwordLengthMask() {
+        val simpleMaskFormatter = SimpleMaskFormatter("NN")
+        val maskTextWatcher = MaskTextWatcher(editTextPasswordLength, simpleMaskFormatter)
+        editTextPasswordLength.addTextChangedListener(maskTextWatcher)
     }
 }
